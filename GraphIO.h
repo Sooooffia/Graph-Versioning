0eb96914e9a24e438d41bb8e3bf3dd48be5ededd
Bob Guo <bobguo2023@u.northwestern.edu>
2023-02-26 18:42:32 -0600

    eralier changes pushed

--- GraphIO.h
+++ GraphIO.h
@@ -7,14 +7,15 @@
 #include "Graph.hpp"
 
 /**
- * Writes the graph G. Each line contains four integers describing a graph: u, v, stroage, retrieval.
+ * Writes the graph G in PACE2016/PACE2017 format.
  * @param os: ofstream pointing to a file for writing.
  * @param G: IntGraph to write.
  */
 void write_graph(ostream &os, const IntGraph &G) {
-    auto edges = G.get_edges(true);
+    auto edges = G.get_edges(false);
+    os << "p tw " << G.size(false) << " " << edges.size() << endl;
     for (auto &[u,v,w]: edges) {
-        os << u << " " << v << " " << w.storage << " " << w.retrieval << endl;
+        os << u << " " << v << endl;
     }
 }
 
@@ -33,16 +34,29 @@ IntGraph read_graph(istream &is) {
     return IntGraph(edges);
 }
 
-void read_graph_from_file(const string& s, const IntGraph &G) {
-    ofstream os;
-    os.open(s);
-    write_graph(os, G);
+//void write_graph_to_file(const string& s, const IntGraph &G) {
+//    ofstream os;
+//    os.open(s);
+//    write_graph(os, G);
+//}
+//
+//IntGraph read_graph_from_file(const string s) {
+//    ifstream is;
+//    is.open(s);
+//    return read_graph(is);
+//}
+
+void write_graph_to_PACE(const string& name) {
+    string graph_name = "../Experiments/" + name + "-cpp.txt";
+    string output_name = "../meiji-tw-2017/" + name + "-PACE.gr";
+    ifstream graph_file(graph_name);
+    ofstream output_file(output_name);
+    if (!graph_file.is_open())
+        throw logic_error("failed to open file\n");
+
+    auto G = read_graph(graph_file);
+    write_graph(output_file, G);
 }
 
-IntGraph write_graph_to_file(const string s) {
-    ifstream is;
-    is.open(s);
-    return read_graph(is);
-}
 
 #endif //GRAPH_VERSIONING_GRAPHIO_H
